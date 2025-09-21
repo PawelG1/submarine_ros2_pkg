3837b298d342bfc5e06ceb67612fec62fd020101
Pawel Galusza <70816853+PawelG1@users.noreply.github.com>
2025-09-21 20:50:28 +0200

    Update GSCAM_CONFIG to use YUY2 format

--- launch/submarine_launch.py
+++ launch/submarine_launch.py
@@ -23,7 +23,10 @@ def generate_launch_description():
         name='gscam_publisher',
         output='screen',
         #mienna GSCAM_CONFIG tylko dla tego noda
-        additional_env={'GSCAM_CONFIG': "v4l2src device=/dev/video0 io-mode=2 do-timestamp=true ! video/x-raw,format=RGB,width=640,height=480,framerate=30/1 ! appsink"}
+        additional_env={'GSCAM_CONFIG': "v4l2src device=/dev/video0 io-mode=2 do-timestamp=true ! \
+  video/x-raw,format=YUY2,width=640,height=480,framerate=30/1 ! \
+  videoconvert name=convert ! video/x-raw,format=BGR ! \
+  appsink name=appsink sync=false max-buffers=1 drop=true"}
     )
     
     node4 = Node(
