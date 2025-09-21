0b11f38713754c2c5395497df53002cca3eadc54
Pawel Galusza <70816853+PawelG1@users.noreply.github.com>
2025-09-21 20:15:28 +0200

    Add GSCAM node and web video server to launch file

--- launch/submarine_launch.py
+++ launch/submarine_launch.py
@@ -16,8 +16,28 @@ def generate_launch_description():
         name='websocket_bridge',
         output='screen'
     )
+
+    node3 = Node(
+        package='gscam',
+        executable='gscam_node',
+        name='gscam_publisher',
+        output='screen',
+        #mienna GSCAM_CONFIG tylko dla tego noda
+        additional_env={'GSCAM_CONFIG': "v4l2src device=/dev/video0 ! \
+  image/jpeg,width=1280,height=720,framerate=30/1 ! \
+  jpegdec ! videoconvert ! video/x-raw,format=RGB"}
+    )
+    
+    node4 = Node(
+        package='web_video_server',
+        executable='web_video_server',
+        name='web_video_server',
+        output='screen'
+    )
     
     return LaunchDescription([
         node1,
-        node2
+        node2,
+        node3,
+        node4
     ])
